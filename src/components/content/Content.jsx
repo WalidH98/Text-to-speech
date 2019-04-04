5f0d5776de24c8f2db8a968c7c4dffb37f518955
morcillo <morcillomuriel@gmail.com>
2019-04-04 22:23:03 +0200

    last position body

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -9,27 +9,27 @@ class Content extends React.Component {
   render() {
     return (
       <Container>
-        <Row>
+        <Row id="margin1">
           <Col sm={{ size: 12, order: 0, offset: 0 }}>
           <h2>Service de synthèse vocale en ligne gratuit</h2>
           </Col>
         </Row>
-        <Row>
-        <Col sm={{ size: 6, order: 0, offset: 2 }}>
+        <Row id="margin2">
+        <Col sm={{ size: 4, order: 1, offset: 2 }}>
         <Lists />
         </Col>
         </Row>
-        <Row>
+        <Row id="margin3">
         <Col sm={{ size: 12, order: 0, offset: 0 }}>
         <VoiceText />
         </Col>
         </Row>
-        <Row>
-        <Col sm={{ size: 2, order: 0, offset: 8 }}>
+        <Row id="margin4">
+        <Col sm={{ size: 3, order: 0, offset: 7 }}>
         <Button color="secondary" size="lg">Lire</Button>{' '}
         </Col>
         </Row>
-        <Row>
+        <Row id="margin5">
         <Col sm={{ size: 12, order: 0, offset: 0 }}>
           <p>Avec BABEL, saisisez votre texte dans la langue de votre choix et ECOUTEZ !!!
           </p>  
