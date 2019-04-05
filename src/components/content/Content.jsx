5eee48b75cd9501ca07e96305e7b3adcb21b9e6a
morcillo <morcillomuriel@gmail.com>
2019-04-05 10:14:20 +0200

    add margin ok, rectify voicetext.jsx

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -8,24 +8,24 @@ import {Container, Row, Col} from 'reactstrap'
 class Content extends React.Component {
   render() {
     return (
-      <Container>
+      <Container fluid className="noPadding" id="fond">
         <Row id="margin1">
           <Col sm={{ size: 12, order: 0, offset: 0 }}>
           <h2>Service de synthèse vocale en ligne gratuit</h2>
           </Col>
         </Row>
         <Row id="margin2">
-        <Col sm={{ size: 4, order: 1, offset: 2 }}>
+        <Col sm={{ size: 5, order: 0, offset: 1 }}>
         <Lists />
         </Col>
         </Row>
         <Row id="margin3">
-        <Col sm={{ size: 12, order: 0, offset: 0 }}>
+        <Col sm={{ size: 10, order: 0, offset: 1 }}>
         <VoiceText />
         </Col>
         </Row>
         <Row id="margin4">
-        <Col sm={{ size: 3, order: 0, offset: 7 }}>
+        <Col sm={{ size: 3, order: 0, offset: 9 }}>
         <Button color="secondary" size="lg">Lire</Button>{' '}
         </Col>
         </Row>
--- src/components/content/voicetext/voicetext.jsx
+++ src/components/content/voicetext/voicetext.jsx
@@ -3,13 +3,7 @@ import './voicetext.css';
 import { Container, Row, Col } from 'reactstrap';
 
 const VoiceText = () => (
-  <Container>
-    <Row>
-      <Col lg={{size:8,offset:2}} xs="12">
-        <textarea class="center" placeholder="Write your speech here ..." name="speech"/>
-      </Col>
-    </Row>
-  </Container>
+  <textarea class="center" placeholder="Write your speech here ..." name="speech"/>
 );
 
 export default VoiceText;
