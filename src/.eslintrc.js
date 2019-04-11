8d036908a419a76502650eee500bdc3a058f32e4
David <eth3rnit3@outlook.fr>
2019-04-11 12:02:11 +0200

    add eslint code quality

---
+++ src/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "extends": [
+    "react-app",
+    "eslint:recommended"
+  ],
+  "rules": {
+    "indent": ["error", 2],
+    "semi": "error",
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "no-console": "off"
+  }
+}
