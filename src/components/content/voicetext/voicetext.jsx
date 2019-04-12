8b87ae4b26f84d5e2f218c697a967af69221e5e1
Miatack <miatack529@gmail.com>
2019-04-12 11:00:50 +0200

    fix some class errors

--- src/components/content/voicetext/voicetext.jsx
+++ src/components/content/voicetext/voicetext.jsx
@@ -1,15 +1,9 @@
-import React from "react";
+import React from 'react';
 import './voicetext.css';
 import { Container, Row, Col } from 'reactstrap';
 
 const VoiceText = () => (
-  <Container>
-    <Row>
-      <Col lg={{size:8,offset:2}} xs="12">
-        <textarea class="center" placeholder="Write your speech here ..."name="speech"/>
-      </Col>
-    </Row>
-  </Container>
+  <textarea className="center" placeholder="Write your speech here ..." name="speech" />
 );
 
 export default VoiceText;
